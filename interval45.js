b6df53829945e0b65927bf6645e49eb0cfc4a76d
Sam Corcos <sam@corcos.io>
2017-03-19 22:47:43 -0700

    reate interval files

---
+++ interval45.js
@@ -0,0 +1,14 @@
+let containerId
+
+module.exports.ping = (event, context, callback) => {
+  if (!containerId) containerId = awsRequestId
+
+  console.log('*** CONTAINER ID *** ' + containerId)
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: '45 minute intervals',
+    }),
+  })
+}
